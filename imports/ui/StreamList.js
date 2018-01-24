23c0b94bc6650c4c9ea76ad5c52bbad5aaf61997
agsdtm7 <marsono.a2@gmail.com>
2018-01-24 14:11:11 -0500

    render empty item if no stream

--- imports/ui/StreamList.js
+++ imports/ui/StreamList.js
@@ -5,15 +5,19 @@ import { Meteor } from 'meteor/meteor';
 import { Streams } from '../api/streams';
 import StreamListHeader from './StreamListHeader';
 import StreamListItem from './StreamListItem';
+import StreamListEmptyItem from './StreamListEmptyItem';
+
+
 
 export const StreamList = (props) => {
   return (
     <div>
       <StreamListHeader />
+      { props.streams.length === 0 ?  <StreamListEmptyItem /> : undefined }
       {props.streams.map((stream) => {
         return <StreamListItem key={stream._id} stream={stream}/>
       })}
-      Stream List { props.streams.length }
+
     </div>
   );
 };
---
+++ imports/ui/StreamListEmptyItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+
+const StreamListEmptyItem = () => {
+  return (
+    <div>
+      <p>Create a stream to start your own</p>
+    </div>
+
+  );
+}
+
+export default StreamListEmptyItem;
